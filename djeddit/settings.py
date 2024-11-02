f7618c62dde7d47a6e1e70745283fcc944519f62
Barath Vedantham <vedant.barath@gmail.com>
2024-11-02 14:38:05 -0700

    Add user authentication and profile system
    - Create new 'users' app with registration and profile functionality
    - Add authentication views (login, logout, password reset)
    - Implement user registration with custom UserRegisterForm
    - Add user profile page and templates
    - Update navigation bar with auth-dependent links
    - Configure login/logout redirects and email backend
    - Add message alerts to base template

--- djeddit/settings.py
+++ djeddit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'posts-home'
+LOGIN_URL = 'login'
+
+# Email settings (for development)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

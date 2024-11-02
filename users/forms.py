91ad34e86a5df0e5bf3d849d8b390c3c4a94a1b2
Barath Vedantham <vedant.barath@gmail.com>
2024-11-02 15:19:01 -0700

    refactor: strip all styling to bare HTML

--- users/forms.py
+++ users/forms.py
@@ -6,12 +6,6 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Add Bootstrap classes to form fields
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']

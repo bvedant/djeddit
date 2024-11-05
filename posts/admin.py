ad1911592b0c5a556e888e46892fecb7c4e976c2
Barath Vedantham <vedant.barath@gmail.com>
2024-11-04 22:11:03 -0800

    Add admin dashboard controls for Posts and Comments

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_posted')
+    list_filter = ('date_posted', 'author')
+    search_fields = ('title', 'content')
+    readonly_fields = ('id', 'date_posted', 'slug')
+    date_hierarchy = 'date_posted'
+    ordering = ('-date_posted',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'author', 'date_posted', 'edited_at')
+    list_filter = ('date_posted', 'author')
+    search_fields = ('content',)
+    readonly_fields = ('id', 'date_posted')
+    date_hierarchy = 'date_posted'
